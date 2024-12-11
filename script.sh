2788e15671c6f4ef9022af35e34a628a4e576bdb
Marcus <m.lambotte@gmail.com>
2024-12-11 15:32:57 +0100

    script

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#affichage d'un message dans la console
+echo "Veillez saisi l'emplacement de stockage"
+#récupération de la saisie
+read EMPLACEMENT
+
+#déplacement
+cd $EMPLACEMENT 
+mkdir Test
+touch collection.txt
+#Affichage d'un message
+echo "Veillez donner le nom du film à ajouter"
+#récupération de la saisie
+read NOM_DU_FILM
+echo $NOM_DU_FILM>>collection.txt
